16a65e98d653f8d56a53fe4c1ca15fe196ec644f
Ervigio <ejadd_75@hotmail.com>
2023-10-29 21:29:11 +0000

    Realizar ejercicio-05

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí:
+const miPrimerCoche={
+    marca: "Opel",
+    modelo: "Astra H",
+    Color: "Plata Estelar",
+    Año: "2004"
+}
+for (const clave in miPrimerCoche) {
+    console.log(clave + ": " + miPrimerCoche[clave]);
+}
