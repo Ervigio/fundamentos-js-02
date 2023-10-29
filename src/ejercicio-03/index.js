83f42edfed5a3eae116fcd0d3ef8c2fa42acb5ec
Ervigio <ejadd_75@hotmail.com>
2023-10-29 21:06:38 +0000

    Realizar ejercicio-03 y ejercicio-04

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,3 @@
+for (let numeros=2; numeros<=20; numeros+=2){
+    console.log(`Iteración: ${numeros}`)
+}
--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,5 @@
 // Escribe tu código aquí:
+const arrayNombres=["Luz María", "Ervigio Javier", "Jose", "Jonás", "Raúl", "Iván"];
+for (const elemento of arrayNombres) {
+    console.log(elemento);
+}
